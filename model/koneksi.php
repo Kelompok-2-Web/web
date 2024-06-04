25aa251a28849606c1fc80e60958e8cf7d07bed9
lleans <ahmadunnail@gmail.com>
2024-06-04 14:39:41 +0700

    🐛 koneksi lagi

--- model/koneksi.php
+++ model/koneksi.php
@@ -1,9 +1,9 @@
 <?php
-$username = 'root';
-$password = '';
+$username = 'budak';
+$password = 'korporat123';
 $database = 'aplikasi_survey_polinema';
 try {
-    $db = new mysqli('localhost', $username, $password, $database);
+    $db = new mysqli(getenv('DATABASE_HOST') != null ? getenv('DATABASE_HOST') : 'muddy-wave-72367.pktriot.net:22189', $username, $password, $database);
     if ($db->connect_error) {
         die('Connection failed: ' . $db->connect_error);
     }
