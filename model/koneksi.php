f11b21e868670c955f4163a94bfeeb206d6f5bde
AldamaitaSS <pijarheksa02@gmail.com>
2024-06-07 03:48:38 +0700

    localhost

--- model/koneksi.php
+++ model/koneksi.php
@@ -1,10 +1,11 @@
 <?php
-$username = 'budak';
-$password = 'korporat123';
+$username = 'root';
+$password = '';
 $database = 'aplikasi_survey_polinema';
 try {
     // $db = new mysqli(getenv('DATABASE_HOST') != null ? getenv('DATABASE_HOST') : 'muddy-wave-72367.pktriot.net:22189', $username, $password, $database);
-    $db = new mysqli('172.29.145.89', $username, $password, $database);
+    // $db = new mysqli('172.29.145.89', $username, $password, $database);
+    $db = new mysqli('localhost', $username, $password, $database);
     if ($db->connect_error) {
         die('Connection failed: ' . $db->connect_error);
     }
